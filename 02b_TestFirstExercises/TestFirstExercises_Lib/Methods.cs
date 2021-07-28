ca0d537ba341804218c16fa37d035691b7ae0dfb
michael carini <snownkc@icloud.com>
2021-07-28 18:55:31 +0100

    updated vers, 1 test missing

--- 02b_TestFirstExercises/TestFirstExercises_Lib/Methods.cs
+++ 02b_TestFirstExercises/TestFirstExercises_Lib/Methods.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
 
 namespace TestFirstExercises
 {
@@ -8,7 +10,14 @@ namespace TestFirstExercises
         // implement this method so that it returns true if x is greater than or equal to y
         public static bool GreaterEqual(int x, int y)
         {
-            return false;
+            if (x >= y)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+            
         }
 
         // Implement this method so that it take a double as an input,
@@ -16,13 +25,25 @@ namespace TestFirstExercises
         // Return a double rounded to 3 decimal places.
         public static double BODMAS(int inputNumber)
         {
-            return 0;
+            double x = inputNumber;
+            
+            double result = (((x * x) + 101) / 7 ) -4;
+            double finResult = Convert.ToDouble(result);
+
+            return Math.Round(finResult, 3);
+
         }
 
         // implement this method so that it returns true if num is even, otherwise false
         public static bool EvenOdd(int num)
         {
-            return false;
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
 
         // implement this method so that it returns 
@@ -30,21 +51,49 @@ namespace TestFirstExercises
         // that are divisible by either 2 or 5
         public static int SumEvenFive(int max)
         {
-            return -1;
+
+            int sum = 0;
+
+            for (int i = 0; i <= max; i++) {
+
+                if (i % 2 == 0 || i % 5 == 0) {
+
+                    sum += i;
+                    return sum;
+                }
+
+                return sum;
+            }
+
+
+            
+            return sum;
+
         }
 
         // implement this method so it returns true if input is "password"
         // regardless of case
         public static bool CheckInput(string input)
         {
-            return false;
+            string wordToCheck = input.ToLower();
+
+            if (wordToCheck == "password")
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+
         }
 
         // implement this method so it returns the sum
         // of all the numbers in the list
         public static int SumList(List<int> list)
         {
-            return -1;
+
+            int Count = list.Sum();
+            return Count;
         }
     }
 }
